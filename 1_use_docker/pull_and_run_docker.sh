a14ea24b93b1a7b61059b21af45d4e1883e57570
Thom Shaw <t.shaw@uq.edu.au>
2020-06-12 16:32:27 +1000

    Clean up formatting

--- 1_use_docker/pull_and_run_docker.sh
+++ 1_use_docker/pull_and_run_docker.sh
@@ -8,9 +8,8 @@ docker run hello-world
 
 #where are the Docker images located?
 docker images 
-#find image id
-#figure something out here
-#image_id=`docker images | grep hello-world | awk '{print $3}`
+#find image id of hello world
+
 #caching
 docker pull hello-world
 
@@ -21,18 +20,23 @@ docker rmi -f ${image_id}
 
 #Ok, so let's try a real example.
 #First let us check out our example dataset
+
 #https://openneuro.org/datasets/ds000205/versions/00001
 #https://aws.amazon.com/cli/
 #aws s3 sync --no-sign-request s3://openneuro.org/ds000205 ds000205-download/
 
 #Then we pull fmriprep (this will take a while!)
 docker pull poldracklab/fmriprep:latest
+
 #NB you can use the docker fmriprep wrapper but we will be interfacing directly with the Docker image.
 #check in https://fmriprep.readthedocs.io/en/stable/docker.html 
-#https://hub.docker.com/r/poldracklab/fmriprep/tags
-##FIXME I can't get this to work on windows
-docker run -ti --rm -v $HOME/data/BrainHackOHBM2020:/data:ro `
--v $HOME/data/derivatives:/out -v $HOME/data/work:/work `
-poldracklab/fmriprep:latest /data /out/fmriprep-latest participant -w /work
+#https://hub.docker.com/r/poldracklab/fmriprep/tags for the latest versions of fmriprep
+
+docker run -ti --rm `
+-v $HOME/data/BrainHackOHBM2020:/data:ro `
+-v $HOME/data/derivatives:/out `
+-v $HOME/data/work:/work `
+poldracklab/fmriprep:latest /data /out/fmriprep-latest `
+participant -w /work
 
 #If you are on Linux: check fmriprep website!
