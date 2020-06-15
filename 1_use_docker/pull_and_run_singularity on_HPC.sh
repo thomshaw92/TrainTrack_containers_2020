27d4e5453f6fa84c4a005a45848a041a6f845c1c
thomshaw92 <thomasbasilshaw@gmail.com>
2020-06-15 16:57:21 +1000

    small change to format

--- 1_use_docker/pull_and_run_singularity on_HPC.sh
+++ 1_use_docker/pull_and_run_singularity on_HPC.sh
@@ -3,13 +3,13 @@
 #First we need to set up singularity.
 #Most HPCs will use lmod to manage software
 
-module load singularity
+module load singularity/3.5.0
 
 #Then pull the same image from DockerHub. Singularity will handle the conversion from docker for you and you don't need root access to do this :)
 mkdir ${HOME}/images
 singularity build ${HOME}/images/fmriprep-latest.simg docker://poldracklab/fmriprep:latest
 
-singularity run --cleanenv fmriprep.simg \
+singularity run --cleanenv ${HOME}/images/fmriprep.simg \
     path/to/data/dir path/to/output/dir \
     participant \
     --participant-label label
